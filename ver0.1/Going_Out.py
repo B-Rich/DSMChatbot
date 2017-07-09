3d982d09fc4aff0a73d8b47a2a1c857c957428dc
Sangmin <sm372181@gmail.com>
2017-07-09 22:04:16 +0900

    Use DMS ID and PW instead of Cookies
    Use DMS ID and PW instead of Cookies

--- ver0.1/Going_Out.py
+++ ver0.1/Going_Out.py
@@ -1,9 +1,17 @@
 import requests
+from getpass import getpass
 
-#sat_out, sun_out if(true) : 외출, if(false) : 안외출
-sat_out = input("Saturday out : ")
-sun_out = input("Sunday out : ")
-#UserSession을 User_Cookie에 입력
-User_Cookie = input("유저 쿠키 : ")
-cookies = {'UserSession' : User_Cookie}
-req = requests.put('http://dsm2015.cafe24.com/apply/goingout', data = {'sat' : sat_out, 'sun' : sun_out}, cookies = cookies)
+Input_id = input("DMS ID입력 : ")
+Input_pw = getpass("DMS PW입력 : ")
+LOGIN_INFO = {
+    'id' : Input_id,
+    'password' : Input_pw,
+    'remember' : '0'
+    }
+with requests.Session() as s :
+    #입력된 정보로 로그인
+    login_req=s.post('http://dsm2015.cafe24.com/account/login/student', data = LOGIN_INFO)   
+    #sat_out, sun_out if(true) : 외출, if(false) : 안외출
+    sat_out = input("Saturday out : ")
+    sun_out = input("Sunday out : ")
+    req = requests.put('http://dsm2015.cafe24.com/apply/goingout', data = {'sat' : sat_out, 'sun' : sun_out}, cookies = cookies)
--- ver0.1/Return_Home.py
+++ ver0.1/Return_Home.py
@@ -1,8 +1,17 @@
 import requests
+from getpass import getpass
 
-#status 1 : 금요 귀가, 2 : 토요 귀가, 3 : 토요 귀사, 4 : 잔류
-status = int(input("status : "))
-#UserSession을 User_Cookie에 입력
-User_Cookie = input("유저 쿠키 : ")
-cookies = {'UserSession' : User_Cookie}
-req = requests.put('http://dsm2015.cafe24.com/apply/stay', data = {'value' : status}, cookies = cookies)
+Input_id = input("DMS ID입력 : ")
+Input_pw = getpass("DMS PW입력 : ")
+LOGIN_INFO = {
+    'id' : Input_id,
+    'password' : Input_pw,
+    'remember' : '0'
+    }
+with requests.Session() as s :
+    #입력된 정보로 로그인
+    login_req=s.post('http://dsm2015.cafe24.com/account/login/student', data = LOGIN_INFO)
+    #status 1 : 금요 귀가, 2 : 토요 귀가, 3 : 토요 귀사, 4 : 잔류
+    status = int(input("status : "))
+    #잔류신청 status를 request 보냄
+    req = s.put('http://dsm2015.cafe24.com/apply/stay', data = {'value' : status})
